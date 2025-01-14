3ceb42fcdba4c9fa766569de35c6712cb26b6332
ducktrung1712 <daicatrungdepdai@gmail.com>
2025-01-14 11:09:13 +0700

    config-spring-security

--- src/main/java/com/ducktrung/learningspingboot/Service/UserService.java
+++ src/main/java/com/ducktrung/learningspingboot/Service/UserService.java
@@ -11,7 +11,6 @@ import com.ducktrung.learningspingboot.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
